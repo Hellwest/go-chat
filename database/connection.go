843df72d3eb1d70328a8954f4e51488c67885a7d
Vladislav Nikitin <dushekrad@gmail.com>
2021-06-18 17:14:22 +0400

    feat(server): add server with connection to the db

---
+++ database/connection.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client = Connection()
+
+func Connection() *mongo.Client {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://admin:chat@localhost:27017/admin")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+
+	return client
+}
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go_chat/auth"
+	"os"
+
+	db "go_chat/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getPortFromEnv(key, fallback string) string {
+	value := os.Getenv(key)
+
+	if len(value) == 0 {
+		return fallback
+	}
+
+	return fmt.Sprintf(":%s", value)
+}
+
+func main() {
+	port := getPortFromEnv("APP_PORT", ":5000")
+
+	router := gin.Default()
+	auth.Routes(router)
+
+	defer db.Client.Disconnect(context.TODO())
+	router.Run(port)
+}
